5966f30e837dcfc46cfb7c2e82b5ef8cb9737c8e
Tony Magro <tony@twogear.com>
2024-03-08 01:01:31 -0600

    Cleanup config and add a few small plugins

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local font_name = "LigaConsolas Nerd Font"
-local font_size = "h11"
+local font_size = "h12"
 
 _G.neovim_gui_font = font_name .. ":" .. font_size
 vim.opt.guifont = _G.neovim_gui_font
@@ -109,22 +109,84 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
+		-- Detect tabstop and shiftwidth automatically
+		"tpope/vim-sleuth",
+	},
+	{
+		-- "gc" to comment visual regions/lines
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+	{
+		-- Fuzzy Finder (files, lsp, etc)
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			{
+				"nvim-lua/plenary.nvim",
+			},
+			{
+				"nvim-tree/nvim-web-devicons",
+			},
+			{
+				"nvim-telescope/telescope-ui-select.nvim",
+			},
+			{
+				-- Compile and install native fuzzy search if cmake is installed
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				cond = function()
+					return vim.fn.executable("cmake") == 1
+				end,
+			},
+		},
 		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+
+			-- Enable telescope extensions, if they are installed
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+			vim.keymap.set("n", "<leader>/", function()
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "[/] Fuzzily search in current buffer" })
+
+			vim.keymap.set("n", "<leader>s/", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[S]earch [/] in Open Files" })
+
+			vim.keymap.set("n", "<leader>sn", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "[S]earch [N]eovim files" })
 		end,
 	},
 	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-	{
+		-- Neovim-qt GUI setup
 		"equalsraf/neovim-gui-shim",
 		config = function()
 			vim.api.nvim_command("GuiFont! " .. _G.neovim_gui_font)
@@ -134,6 +196,7 @@ require("lazy").setup({
 		end,
 	},
 	{
+		-- Treesitter-based highlighting and indenting
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
@@ -160,34 +223,70 @@ require("lazy").setup({
 		end,
 	},
 	{
+    -- Lightweight code formatter
 		"stevearc/conform.nvim",
-		opts = {},
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				json = { { "prettierd", "prettier" } },
+				markdown = { { "prettierd", "prettier" } },
+				javascript = { { "prettierd", "prettier" } },
+			},
+		},
 	},
 	{
+		-- Buffer Tabs
 		"akinsho/bufferline.nvim",
 		version = "*",
 		dependencies = "nvim-tree/nvim-web-devicons",
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
+		config = function()
+			require("bufferline").setup({})
 		end,
-		opts = {},
 	},
 	{
+		-- Displays popup with possible key bindings for the command you started typing
+		"folke/which-key.nvim",
+		event = "VimEnter",
+		config = function()
+			require("which-key").setup()
+
+			-- Document existing key chains
+			require("which-key").register({
+				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			})
+		end,
+	},
+	{
+		-- tokyonight theme
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
 	},
+	{
+		-- Fancy statusline
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "tokyonight",
+				},
+			})
+		end,
+	},
 })
 
 -- OSX Terminal.app only supports 256 colors
 if vim.env.TERM_PROGRAM == "Apple_Terminal" then
-	-- ginit.vim will override these when nvim-qt is launched from Terminal.app
+	-- ginit.vim will correctly override these when nvim-qt is launched from Terminal.app
 	vim.opt.termguicolors = false
 	vim.cmd.colorscheme("habamax")
 else
@@ -195,24 +294,7 @@ else
 	vim.cmd.colorscheme("tokyonight")
 end
 
-require("lualine").setup({
-	options = {
-		theme = "tokyonight",
-	},
-})
-
-require("bufferline").setup({})
-
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "black" },
-		json = { { "prettierd", "prettier" } },
-		markdown = { { "prettierd", "prettier" } },
-		javascript = { { "prettierd", "prettier" } },
-	},
-})
-
+-- Format command for format current buffer
 vim.api.nvim_create_user_command("Format", function(args)
 	local range = nil
 	if args.count ~= -1 then
--- install_apps.py
+++ install_apps.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
         "ripgrep",
         "stylua",
         "tree-sitter",
+        "cmake",
     ]
     darwin_apps = [
         "black",
