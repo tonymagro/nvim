1d8fe2c28fde7a240e447b7d4eb7cd2e64924f7f
Tony Magro <tony@twogear.com>
2024-03-10 10:08:50 -0500

    Fix close tab

--- init.lua
+++ init.lua
@@ -104,6 +104,84 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Common kill function for bdelete and bwipeout
+-- credits: based on bbye and nvim-bufdel
+---@param kill_command? string defaults to "bd"
+---@param bufnr? number defaults to the current buffer
+---@param force? boolean defaults to false
+local function buf_kill(kill_command, bufnr, force)
+  kill_command = kill_command or "bd"
+
+  local bo = vim.bo
+  local api = vim.api
+  local fmt = string.format
+  local fn = vim.fn
+
+  if bufnr == 0 or bufnr == nil then
+    bufnr = api.nvim_get_current_buf()
+  end
+
+  local bufname = api.nvim_buf_get_name(bufnr)
+
+  if not force then
+    local choice
+    if bo[bufnr].modified then
+      choice = fn.confirm(fmt([[Save changes to "%s"?]], bufname), "&Yes\n&No\n&Cancel")
+      if choice == 1 then
+        vim.api.nvim_buf_call(bufnr, function()
+          vim.cmd("w")
+        end)
+      elseif choice == 2 then
+        force = true
+      else
+        return
+      end
+    elseif api.nvim_buf_get_option(bufnr, "buftype") == "terminal" then
+      choice = fn.confirm(fmt([[Close "%s"?]], bufname), "&Yes\n&No\n&Cancel")
+      if choice == 1 then
+        force = true
+      else
+        return
+      end
+    end
+  end
+
+  -- Get list of windows IDs with the buffer to close
+  local windows = vim.tbl_filter(function(win)
+    return api.nvim_win_get_buf(win) == bufnr
+  end, api.nvim_list_wins())
+
+  if force then
+    kill_command = kill_command .. "!"
+  end
+
+  -- Get list of active buffers
+  local buffers = vim.tbl_filter(function(buf)
+    return api.nvim_buf_is_valid(buf) and bo[buf].buflisted
+  end, api.nvim_list_bufs())
+
+  -- If there is only one buffer (which has to be the current one), vim will
+  -- create a new buffer on :bd.
+  -- For more than one buffer, pick the previous buffer (wrapping around if necessary)
+  if #buffers > 1 and #windows > 0 then
+    for i, v in ipairs(buffers) do
+      if v == bufnr then
+        local prev_buf_idx = i == 1 and #buffers or (i - 1)
+        local prev_buffer = buffers[prev_buf_idx]
+        for _, win in ipairs(windows) do
+          api.nvim_win_set_buf(win, prev_buffer)
+        end
+      end
+    end
+  end
+
+  -- Check if buffer still exists, to ensure the target buffer wasn't killed
+  -- due to options like bufhidden=wipe.
+  if api.nvim_buf_is_valid(bufnr) and bo[bufnr].buflisted then
+    vim.cmd(string.format("%s %d", kill_command, bufnr))
+  end
+end
+
 -- Bootstrap Lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -512,7 +590,7 @@ require('lazy').setup {
     opts = {},
   },
   {
-    -- Tabs
+    -- Buffer Tabs
     'akinsho/bufferline.nvim',
     version = '*',
     dependencies = {
@@ -521,6 +599,15 @@ require('lazy').setup {
     config = function()
       require('bufferline').setup {
         options = {
+          close_command = function(bufnr)
+            buf_kill("bd", bufnr, false)
+          end,
+          max_name_length = 18,
+          max_prefix_length = 15,
+          truncate_names = true,
+          tab_size = 18,
+          separator_style = "thin",
+          always_show_bufferline = false,
           offsets = {
             {
               filetype = "NvimTree",
@@ -541,7 +628,6 @@ require('lazy').setup {
               padding = 1,
             },
           },
-          always_show_bufferline = false,
         },
       }
     end,
