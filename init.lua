27fc8cda4be2618968d8e9b202735f8d9e8e99eb
Tony Magro <tony@twogear.com>
2024-03-13 01:35:52 -0500

    Add org mode

--- init.lua
+++ init.lua
@@ -763,6 +763,31 @@ require('lazy').setup {
   {
     -- Highlight other uses of the word under the cursor
     "RRethy/vim-illuminate",
+  },
+  {
+    'nvim-orgmode/orgmode',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter', lazy = true },
+    },
+    event = 'VeryLazy',
+    config = function()
+      -- Load treesitter grammar for org
+      require('orgmode').setup_ts_grammar()
+
+      -- Setup treesitter
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+        },
+        ensure_installed = { 'org' },
+      })
+
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = '~/orgfiles/**/*',
+        org_default_notes_file = '~/orgfiles/refile.org',
+      })
+    end,
   }
 }
 
