a1fd7ede719aa105b7fde03cc3bc0545c1aef95c
Tony Magro <tony@twogear.com>
2024-03-14 20:05:35 -0500

    Bindings and disable mini

--- init.lua
+++ init.lua
@@ -340,6 +340,7 @@ require('lazy').setup {
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('<A-g>', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
@@ -674,28 +675,6 @@ require('lazy').setup {
       'RainbowDelimQuoted',
       'RainbowMultiDelim',
     },
-  },
-  {
-    -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      -- require('mini.surround').setup()
-
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
   },
   {
     -- Treesitter-based highlighting and indenting
